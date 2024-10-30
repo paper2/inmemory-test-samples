e69755b3a771beea8243a3545fba4d4e5a4099ff
Yohei Kamitsukasa <35333687+paper2@users.noreply.github.com>
2024-10-30 20:48:23 +0900

    fetchのスパンは取得できたやつ

--- inMemorySpanExporterSample.test.ts
+++ inMemorySpanExporterSample.test.ts
@@ -1,45 +1,37 @@
-import * as assert from 'assert';
 import {
   ConsoleSpanExporter,
   InMemorySpanExporter,
   SimpleSpanProcessor,
   Tracer,
 } from '@opentelemetry/sdk-trace-node'
-import request from 'supertest'
-import { context, trace } from '@opentelemetry/api';
 import http from 'http';
 import { initializeSDK } from "./instrumentation";
 // INMPORTANT: invoke before import app
 // NodeSDK not supporting forceFlush then shuld use SimpleSpanProcessor for get spans immediately.
 const memoryExporter = new InMemorySpanExporter();
-initializeSDK(new SimpleSpanProcessor(new ConsoleSpanExporter))
+initializeSDK(new SimpleSpanProcessor(memoryExporter))
 
 import app from "./app";
 
+let server: http.Server;
 
-/**
- * 指定時間処理を停止する関数
- * @param {number} ms 待機するミリ秒数
- */
-async function sleep(ms: number) {
-  return new Promise((resolve) => setTimeout(resolve, ms));
-}
+beforeAll((done) => {
+  server = app.listen(3000, () => done());
+});
 
+beforeEach(() => {
+  memoryExporter.reset()
+});
 
-describe('InMemorySpanExporter', () => {
-  beforeEach(() => {
-    memoryExporter.reset()
-  });
+afterAll((done) => {
+  server.close(done);
+});
 
-  it('should get finished spans', async () => {
-    request(app)
-      .get('/rolldice')
-      .expect(200)
-      .end(function (err, res) {
-        if (err) throw err;
-      })
-    await sleep(1000)
-    const spans = memoryExporter.getFinishedSpans()
-    expect(spans).toEqual("hoge")
-  });
+test('GET /hello should return Hello, World!', async () => {
+  // 実際に成功はしている
+  const response = await fetch('http://localhost:3000/rolldice');
+  expect(response.status).toBe(200);
+  const spans = memoryExporter.getFinishedSpans()[0]
+  // これfetchの自動計装が出てくる。app側のトレースは出てきてなさそう。。。
+  expect(spans).toMatchObject({ hoge: "test" })
 });
